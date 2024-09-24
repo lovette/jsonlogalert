4e6dd40b2b595cd115fd1c64e34e51f0fa51020b
Lance Lovette <lance.lovette@gmail.com>
2024-09-24 09:43:45 -0400

    Raise `LogAlertConfigError` if YAML file contains duplicate directives.

--- src/jsonlogalert/utils.py
+++ src/jsonlogalert/utils.py
@@ -3,10 +3,50 @@ from __future__ import annotations
 import json
 import logging
 from pathlib import Path
+from typing import TYPE_CHECKING
 
 import yaml
 
 from jsonlogalert.exceptions import LogAlertConfigError
+
+if TYPE_CHECKING:
+    from io import TextIOWrapper
+
+######################################################################
+# UniqueKeyLoader
+
+
+# Thanks to the conversation at:
+# https://gist.github.com/pypt/94d747fe5180851196eb
+
+
+class UniqueKeyLoader(yaml.SafeLoader):
+    """Load YAML and fail if key is duplicated."""
+
+    def construct_mapping(self, node: yaml.MappingNode, deep: bool = False) -> dict:
+        """Internal mapping function.
+
+        Args:
+            node (yaml.MappingNode): ??
+            deep (bool): ??
+
+        Raises:
+            ValueError: Duplicate key found.
+
+        Returns:
+            dict
+        """
+        mapping = set()
+
+        for key_node, _value_node in node.value:
+            if ":merge" not in key_node.tag:
+                key = self.construct_object(key_node, deep=deep)
+                if key in mapping:
+                    raise ValueError(f"{key!r}: Duplicate key found in YAML.")
+                mapping.add(key)
+
+        return super().construct_mapping(node, deep)
+
 
 ######################################################################
 # Helper functions
@@ -26,13 +66,25 @@ def read_config_file(config_path: Path) -> dict | None:
     """
     config = None
 
+    def _read_json_config(fp: TextIOWrapper) -> None:
+        try:
+            return json.load(fp)
+        except json.JSONDecodeError as err:
+            raise LogAlertConfigError(f"{err}") from err
+
+    def _read_yaml_config(fp: TextIOWrapper) -> None:
+        try:
+            return yaml.load(fp.read(), Loader=UniqueKeyLoader)  # noqa: S506
+        except (yaml.YAMLError, ValueError) as err:
+            raise LogAlertConfigError(f"{err}") from err
+
     try:
         if config_path.is_file() and config_path.stat().st_size:
             logging.debug(f"Reading {config_path}")
             with config_path.open() as fp:
-                config = json.load(fp) if config_path.suffix == ".json" else yaml.safe_load(fp.read())
+                config = _read_json_config(fp) if config_path.suffix == ".json" else _read_yaml_config(fp)
 
-    except (OSError, json.JSONDecodeError, yaml.YAMLError) as err:
+    except OSError as err:
         raise LogAlertConfigError(f"{err}") from err
 
     return config
